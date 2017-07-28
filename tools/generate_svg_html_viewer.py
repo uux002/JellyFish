4de21ddb5865a84d47a6555eaa196cf81cf13452
FredShao <fredhsao@aliyun.com>
2017-07-28 18:58:02 +0800

    Update tools

--- tools/generate_svg_html_viewer.py
+++ tools/generate_svg_html_viewer.py
@@ -14,6 +14,9 @@ with open('svg_viewer.html','w') as f:
 	f.write("<html>\n")
 	f.write("<body>\n")
 	for svg in svglist:
+		f.write("<div>")
 		f.write('<img src="%s" width="100" height="100" name="%s"/>\n' % (svg, svg))
+		f.write("<span>%s" % svg)
+		f.write("</div>")
 	f.write("</body>\n")
 	f.write("</html>\n")
