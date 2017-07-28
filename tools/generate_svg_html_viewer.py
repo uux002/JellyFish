1dfbc3e78f91086f5e960898118eb7b919001591
FredShao <fredhsao@aliyun.com>
2017-07-28 18:49:52 +0800

    Add Tools

---
+++ tools/generate_svg_html_viewer.py
@@ -0,0 +1,19 @@
+import os
+
+svglist = []
+
+for x in os.listdir('.'):
+	if os.path.isfile(x) and os.path.splitext(x)[1] == '.svg':
+		#print(x)
+		svglist.append(x)
+
+for svg in svglist:
+	print(svg)
+
+with open('svg_viewer.html','w') as f:
+	f.write("<html>\n")
+	f.write("<body>\n")
+	for svg in svglist:
+		f.write('<img src="%s" width="100" height="100" name="%s"/>\n' % (svg, svg))
+	f.write("</body>\n")
+	f.write("</html>\n")
